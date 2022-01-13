af5cfdd60d109c36a5f8e9944b13d8db07c5a341
Francis Bond <francisbond1996@gmail.com>
2022-01-12 23:52:12 -0600

    Added start html page

---
+++ Assets/js/script.js
@@ -0,0 +1,9 @@
+// Pseudocode
+    // Make dashboard in HTML
+        // input for my location
+        // search(submit button)
+            // fetch info from api
+            // take the input value
+            // use an if statement to change color of the UV index depending on conditions
+            
+    // 
